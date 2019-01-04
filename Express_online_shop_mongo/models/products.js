c9ffa4393d4001cfbcf033a9fb9d4b08fa1b04e4
rahul <rahulkr009@gmail.com>
2019-01-04 00:31:51 -0800

    start adding mongodb in project

---
+++ Express_online_shop_mongo/models/products.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require("path");
+const rootPath = require('../util/path');
+const Cart = require('./cart');
+
+module.exports = class Products {
+    constructor(title, imageUrl, description, price) {
+        this.id = Math.random().toString();
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+    }
+
+    static edit(productId, product, cbFunc) {
+    
+    }
+
+    static delete(productId, cbFunc) {
+    }
+
+    static getAll(cbFunc) {
+
+    }
+
+    static getProductById(id, cbFunc) {
+    }
+}
