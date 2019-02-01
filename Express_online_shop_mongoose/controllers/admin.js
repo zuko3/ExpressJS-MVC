51c9c46eab0ce37578631e28ae52778f4c31651a
rahul <rahulkr009@gmail.com>
2019-02-01 11:51:27 -0800

    adding the admin functionalities

--- Express_online_shop_mongoose/controllers/admin.js
+++ Express_online_shop_mongoose/controllers/admin.js
@@ -19,7 +19,7 @@ exports.addProducts = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Products.fetchAll()
+    Products.find()
         .then(products => res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
@@ -28,38 +28,43 @@ exports.getProducts = (req, res, next) => {
         .catch(err => console.log("[ Error in getProducts ]:", err))
 }
 
-// exports.getEditProduct = (req, res, next) => {
-//     const editMode = (req.query.edit === 'true');
-//     if (!editMode) {
-//         return res.redirect("/")
-//     }
-//     const { productId } = req.params;
-//     Products.findById(productId)
-//         .then(product => {
-//             if (!product) {
-//                 return res.redirect("/");
-//             }
-//             res.render('admin/edit-product', {
-//                 pageTitle: 'Edit Product',
-//                 path: '/admin/edit-product',
-//                 editing: editMode,
-//                 product: product
-//             });
-//         }).catch(err => console.log("[ Error in getEditProduct ]:", err))
-// }
+exports.getEditProduct = (req, res, next) => {
+    const editMode = (req.query.edit === 'true');
+    if (!editMode) {
+        return res.redirect("/")
+    }
+    const { productId } = req.params;
+    Products.findById(productId)
+        .then(product => {
+            if (!product) {
+                return res.redirect("/");
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        }).catch(err => console.log("[ Error in getEditProduct ]:", err))
+}
 
-// exports.postEditProduct = (req, res, next) => {
-//     const { productId, title, imageUrl, price, description } = req.body;
-//     const Product = new Products(title, imageUrl, description, price);
-//     Product.edit(productId)
-//         .then(() => res.redirect("/admin/products"))
-//         .catch(err => console.log("[Error in postEditProduct]:", err))
-// }
+exports.postEditProduct = (req, res, next) => {
+    const { productId, title, imageUrl, price, description } = req.body;
+    Products.findById(productId)
+        .then(product => {
+            product.title = title;
+            product.imageUrl = imageUrl;
+            product.price = price;
+            product.description = description;
+            return product.save()
+        })
+        .then(() => res.redirect("/admin/products"))
+        .catch(err => console.log("[Error in postEditProduct]:", err))
+}
 
-
-// exports.postDeleteProduct = (req, res, next) => {
-//     const { productId } = req.body;
-//     Products.delete(productId)
-//     .then(() => res.redirect("/admin/products"))
-//     .catch(err=>console.log("[Catch inside postDeleteProduct ]:",err))
-// } 
+exports.postDeleteProduct = (req, res, next) => {
+    const { productId } = req.body;
+    Products.findByIdAndRemove(productId)
+    .then(() => res.redirect("/admin/products"))
+    .catch(err=>console.log("[Catch inside postDeleteProduct ]:",err))
+} 
