101fa564587be529edab237038e804b75f85e0b3
rahul <rahulkr009@gmail.com>
2019-02-06 13:14:00 -0800

    route file upadte

--- Express_online_shop_mongoose/Routes/shop.js
+++ Express_online_shop_mongoose/Routes/shop.js
@@ -8,8 +8,8 @@ router.get("/products/:productId",shopController.getProduct)
 router.get("/cart",shopController.getCart);
 router.post("/cart",shopController.postCart);
 router.post("/cart-delete-item",shopController.postCartDelete)
-// router.post('/create-order',shopController.postOrders)
-// router.get("/orders",shopController.getOrders);
-// router.get("/checkout",shopController.getCheckout);
+router.post('/create-order',shopController.postOrders)
+router.get("/orders",shopController.getOrders);
+router.get("/checkout",shopController.getCheckout);
 
 module.exports = router;
