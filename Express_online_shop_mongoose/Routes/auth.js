2f1578ee3065d8c6e72b75eb9d55e6506e3d3ab9
rahul <rahulkr009@gmail.com>
2019-02-10 22:23:36 -0800

    adding validation and custom validatons

--- Express_online_shop_mongoose/Routes/auth.js
+++ Express_online_shop_mongoose/Routes/auth.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const authController = require('../controllers/auth');
 const router = express.Router();
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
+//Checks checks of field in entire request, while body checks in only body
 
 router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
-router.post('/signup', check('email').isEmail().withMessage("please enter a valid email"), authController.postSignup);
+
+router.post('/signup', [
+    check('email').isEmail()
+        .withMessage("please enter a valid email"),
+    body('password').isLength({ min: 5 })
+        .isAlphanumeric()
+        .withMessage("please enter a passowrd of text and chacter of 5 chars"),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('password have to match!')
+        }
+        return true
+    })
+        .withMessage("password have to match!")
+], authController.postSignup);
+
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
